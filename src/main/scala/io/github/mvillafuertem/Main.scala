3d1458e353398e4039d5bab482aa0eb7e9d7993d
Miguel Villafuerte <mivimur@hotmail.com>
2023-01-16 16:18:13 +0100

    test

--- src/main/scala/io/github/mvillafuertem/Main.scala
+++ src/main/scala/io/github/mvillafuertem/Main.scala
@@ -4,14 +4,25 @@ import cats.effect.{ ExitCode, IO, IOApp }
 import io.github.mvillafuertem.user.User
 import neotypes.GraphDatabase
 import neotypes.cats.effect.implicits._
-// import neotypes.generic.DerivedResultMapper.deriveResultMapper
-import neotypes.generic.auto._
+import neotypes.generic.DerivedResultMapper.deriveResultMapper
+import neotypes.mappers.ValueMapper
+import org.neo4j.driver.Value
+import scalapb.UnknownFieldSet
+// import neotypes.generic.auto._
 import neotypes.implicits.all._
 import org.neo4j.driver.AuthTokens
 
 object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
+
+    // implicit val valueMapper: ValueMapper[UnknownFieldSet] = semiauto.deriveUnwrappedValueMapper
+
+    implicit val test = new ValueMapper[UnknownFieldSet] {
+      override def to(fieldName: String, value: Option[Value]): Either[Throwable, UnknownFieldSet] =
+        // only for test
+        Right(UnknownFieldSet.empty)
+    }
 
     val user = User.of(name = "Pepe", surname = "Test")
     println(user)
