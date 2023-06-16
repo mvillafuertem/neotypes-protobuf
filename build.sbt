a32d98339e158638bd3ddf4ca2a55ec0a8f2ca39
Miguel Villafuerte <mivimur@hotmail.com>
2023-06-16 18:42:39 +0200

    update version

--- build.sbt
+++ build.sbt
@@ -6,10 +6,10 @@ lazy val root = (project in file("."))
   .settings(
     name                 := "neotypes-protobuf",
     libraryDependencies ++= Seq(
-      "org.neo4j.driver"    % "neo4j-java-driver"    % "5.4.0",
-      "io.github.neotypes" %% "neotypes-cats-effect" % "0.23.1",
-      "io.github.neotypes" %% "neotypes-generic"     % "0.23.1",
-      "org.typelevel"      %% "cats-effect"          % "3.3.5"
+      "org.neo4j.driver"    % "neo4j-java-driver"    % "5.6.0",
+      "io.github.neotypes" %% "neotypes-cats-effect" % "1.0.0-M1",
+      "io.github.neotypes" %% "neotypes-generic"     % "1.0.0-M1",
+      "org.typelevel"      %% "cats-effect"          % "3.4.8"
     ),
     Compile / PB.targets := Seq(
       scalapb.gen() -> (Compile / sourceManaged).value / "scalapb"
--- src/main/scala/io/github/mvillafuertem/Main.scala
+++ src/main/scala/io/github/mvillafuertem/Main.scala
@@ -1,39 +1,51 @@
 package io.github.mvillafuertem
 
 import cats.effect.{ ExitCode, IO, IOApp }
+import cats.syntax.option.none
 import io.github.mvillafuertem.user.User
 import neotypes.GraphDatabase
 import neotypes.cats.effect.implicits._
-import neotypes.generic.DerivedResultMapper.deriveResultMapper
-import neotypes.mappers.ValueMapper
-import org.neo4j.driver.Value
-import scalapb.UnknownFieldSet
-// import neotypes.generic.auto._
-import neotypes.implicits.all._
+import neotypes.mappers.ResultMapper
+import neotypes.model.types.Value
+import neotypes.syntax.all._
 import org.neo4j.driver.AuthTokens
+import scalapb.UnknownFieldSet
 
 object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
 
-    // implicit val valueMapper: ValueMapper[UnknownFieldSet] = semiauto.deriveUnwrappedValueMapper
-
-    implicit val test = new ValueMapper[UnknownFieldSet] {
-      override def to(fieldName: String, value: Option[Value]): Either[Throwable, UnknownFieldSet] =
-        // only for test
-        Right(UnknownFieldSet.empty)
+    @scala.annotation.unused
+    implicit val valueMapper: ResultMapper[UnknownFieldSet] = ResultMapper.fromMatch { case Value.Str(value) =>
+      // only for test
+      println(value)
+      Right(UnknownFieldSet.empty)
     }
 
-    val user = User.of(name = "Pepe", surname = "Test")
+    // implicit val valueMapper = ResultMapper.fromValueMapper[UnknownFieldSet]
+
+    val user = User.of(name = Some("Pepe"), surname = Some("Test"), username = Some("pp"))
     println(user)
 
     GraphDatabase
-      .driver[IO](
+      .asyncDriver[IO](
         "neo4j://localhost:7687",
         AuthTokens.basic("neo4j", "accounttest")
       )
       .use { driver =>
-        "MATCH (u: User) RETURN u LIMIT 1".readOnlyQuery[User].single(driver)
+//        implicit val test = new ValueMapper[UnknownFieldSet] {
+//          override def to(fieldName: String, value: Option[Value]): Either[Throwable, UnknownFieldSet] =
+//            // only for test
+//            Right(UnknownFieldSet.empty)
+//        }
+
+        """MATCH (u:User {name: "hola"}) RETURN u LIMIT 1"""
+          .query(ResultMapper.fromFunction(User.apply _))
+          .single(driver)
+          .map { user =>
+            println(user); user
+          }
+          .handleError(_ => println(none))
       }
       .as(ExitCode.Success)
   }
